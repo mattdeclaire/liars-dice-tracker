1eed2bc7c12ffbd94a3855c76b64483829e63127
Matt DeClaire <matt@declaire.com>
2026-01-03 15:43:13 -0800

    Initial Commit

---
+++ Liar's Dice Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Liar's Dice Tracker
+//
+//  Created by Matt DeClaire on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Liar's Dice Tracker/Liar_s_Dice_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liar_s_Dice_TrackerApp.swift
+//  Liar's Dice Tracker
+//
+//  Created by Matt DeClaire on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Liar_s_Dice_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
